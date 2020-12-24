78e51e65f39ea1b13f9e58884aa81a4356b99092
dougfair <doug.fairlie@onjcri.org.au>
2020-12-24 22:49:40 +1100

    changes to routes

--- app.js
+++ app.js
@@ -59,10 +59,10 @@ app.use(projectRoutes);
 app.use(labphotoRoutes);
 
 
-app.use(express.static(path.join(__dirname, "client", "build"))
+app.use(express.static(path.join(__dirname, "./client/build"))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 
