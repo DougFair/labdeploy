72b06f25383ad5fc1cfe32c32b2b871dbe5682b5
dougfair <doug.fairlie@onjcri.org.au>
2020-12-28 16:33:02 +1100

    changes to routes

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const methodOverride = require("method-override");
 require('dotenv').config();
 var cors = require('cors')
 const path = require("path")
- 
-app.use(cors())
-// Requiring models
-const Photo = require("./models/Photo.js");
-const PDFfile = require("./models/PDFfile.js");
 
-// const upload = require('./imageUpload');
+app.use(cors())
+
+
+// Static file pathways when not using Amazon S3
+app.use(express.static(__dirname, + "/public"));
+app.use("/images",express.static(__dirname, + "/images"));
+app.use("/pdf", express.static(__dirname, + "/pdf"));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json())
+app.use(methodOverride("_method"));
+
+
 
 // Requiring routes
 const homeRoutes = require("./routes/home");
@@ -20,41 +26,20 @@ const mediaRoutes = require("./routes/media");
 const searchRoutes = require("./routes/search");
 const adminRoutes = require("./routes/admin")
 const siteinfoRoutes = require("./routes/siteinfo")
-// app.use("/siteinfo", siteinfoRoutes);
+app.use("/siteinfo", siteinfoRoutes);
 const blogRoutes = require("./routes/blog")
-
 const teamRoutes = require("./routes/team")
 const labdocumentRoutes = require("./routes/labdocuments")
 const projectRoutes = require("./routes/project")
 const labphotoRoutes = require("./routes/pics")
 
-// app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-app.use(bodyParser.json())
-app.use(methodOverride("_method"));
-
-// Static file pathways when not using Amazon S3
-app.use(express.static(__dirname, + "/public"));
-app.use("/images",express.static(__dirname, + "/images"));
-app.use("/pdf", express.static(__dirname, + "/pdf"));
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-
-}
-
-
-app.set("view engine", "ejs");
 
 // Using routes
+// app.use(siteinfoRoutes);
 app.use(homeRoutes);
 app.use(mediaRoutes);
 app.use(searchRoutes);
 app.use(adminRoutes);
-app.use(siteinfoRoutes);
 app.use(blogRoutes);
 app.use(teamRoutes);
 app.use(labdocumentRoutes);
@@ -67,9 +52,7 @@ if (process.env.NODE_ENV === 'production'){
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-
 }
-
 
 let PORT = process.env.PORT || 8000
 
--- client/src/App.js
+++ client/src/App.js
@@ -74,8 +74,8 @@ class App extends Component {
 
 
   componentDidMount () {
-  axios.post("/siteInfo", {keysa: "kjdfjkhfdjhk"})
-    axios.get("/siteInfo")
+  axios.post("/siteinfo/siteInfo", {keysa: "kjdfjkhfdjhk"})
+    axios.get("/siteinfo/siteInfo")
     .then(response => {
       if (response.data[0].labName){
   
